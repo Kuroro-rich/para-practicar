52984fd17680ca351b7ffe394721aeabb7494cf0
notGabo <ga.soto@duocuc.cl>
2025-05-01 17:51:20 -0400

    FIX variable DBPASSWORD

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const upload = multer();
 const db = mysql.createConnection({
   host: process.env.DBHOST,
   user: process.env.DBUSER,
-  password: process.env.DBPASS,
+  password: process.env.DBPASSWORD,
   database: process.env.DBNAME,
 });
 
